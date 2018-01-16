2e77e2c265061501ab3a9f215ef15ac5120ca8e9
vagrant <vagrant@vagrant.vm>
2018-01-16 01:01:54 +0000

    Node.js express simple web app tutorial. Everything completed up to file upload form and cookie management.

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-Parser');
+
+//Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+  res.sendFile(__dirname + "/" + "index.html");
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+  //Prepare output in JSON format
+  response = {
+    first_name:req.body.first_name,
+    last_name:req.body.last_name
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+})
 
 //This response with "Hello world" on the homepage
 app.get('/', function(req, res) {
